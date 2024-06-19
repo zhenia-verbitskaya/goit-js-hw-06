bd61add3251533fce74577a24172673ad997d602
Zhenia Verbitskaya <jvzns17@gmail.com>
2024-06-19 13:09:58 +0300

    add 3 tasks

---
+++ js/task-2.js
@@ -0,0 +1,32 @@
+class Storage {
+  #items;
+
+  constructor(params) {
+    this.#items = params;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
+
+storage.addItem("Droid");
+console.log(storage.getItems());
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
+
+storage.removeItem("Scaner");
+console.log(storage.getItems());
